53e19024a9bd772286a1ba62d2490e67a82470ea
KatyaMy <20FamGam20@gmail.com>
2022-11-07 18:25:24 -0500

    fix 24

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -7,16 +7,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 # def driver():
-    # # service = ChromeService(executable_path=ChromeDriverManager().install())
-    # # driver = webdriver.Chrome(service=service)
-    # yield driver
-    # driver.quit()
+# # service = ChromeService(executable_path=ChromeDriverManager().install())
+# # driver = webdriver.Chrome(service=service)
+# yield driver
+# driver.quit()
+
 
 def driver():
     o = webdriver.ChromeOptions()
     o.headless = True
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),options=o
+        service=Service(ChromeDriverManager().install()), options=o
     )
     yield driver
     driver.quit()
--- Tests/test_login_main_page.py
+++ Tests/test_login_main_page.py
@@ -4,14 +4,14 @@ from selenium.webdriver.common.by import By
 
 
 def test_open_web_page(driver):
-    driver.get('https://www.saucedemo.com/')
+    driver.get("https://www.saucedemo.com/")
 
-    assert driver.title == 'Swag Labs'
+    assert driver.title == "Swag Labs"
     time.sleep(2)
 
 
 def test_login_page(driver):
-    driver.get('https://www.saucedemo.com/')
+    driver.get("https://www.saucedemo.com/")
     login_box = driver.find_element(By.ID, "user-name")
     login_box.send_keys("standard_user")
     password_box = driver.find_element(By.ID, "password")
@@ -19,7 +19,7 @@ def test_login_page(driver):
     button = driver.find_element(By.ID, "login-button")
     button.click()
     time.sleep(3)
-    if driver.title == 'Swag Labs':
+    if driver.title == "Swag Labs":
         print("CORRECT!")
     else:
         print("WRONG")
