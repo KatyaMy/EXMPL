cb1103baa3164c8faab8f9c3bb7994887183bece
KatyaMy <20FamGam20@gmail.com>
2022-11-07 15:43:31 -0500

    fix11

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -6,9 +6,17 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="function")
+# def driver():
+    # # service = ChromeService(executable_path=ChromeDriverManager().install())
+    # # driver = webdriver.Chrome(service=service)
+    # yield driver
+    # driver.quit()
+
 def driver():
     o = webdriver.ChromeOptions()
     o.headless = True
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),options=o
+    )
     yield driver
     driver.quit()
