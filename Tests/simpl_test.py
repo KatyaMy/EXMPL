12c22eed8fc98825312cd8004a577ef8a87d3427
KatyaMy <20FamGam20@gmail.com>
2022-11-07 14:25:47 -0500

    fix9

---
+++ Tests/simpl_test.py
@@ -0,0 +1,26 @@
+import time
+import pytest
+from selenium.webdriver.common.by import By
+
+
+def test_open_web_page(driver):
+    driver.get('https://www.saucedemo.com/')
+
+    assert driver.title == 'Swag Labs'
+    time.sleep(2)
+
+
+def test_login_page(driver):
+    driver.get('https://www.saucedemo.com/')
+    login_box = driver.find_element(By.ID, "user-name")
+    login_box.send_keys("standard_user")
+    password_box = driver.find_element(By.ID, "password")
+    password_box.send_keys("secret_sauce")
+    button = driver.find_element(By.ID, "login-button")
+    button.click()
+    time.sleep(3)
+    if driver.title == 'Swag Labs':
+        print("CORRECT!")
+    else:
+        print("WRONG")
+    driver.quit()
---
+++ pages/locators.py
@@ -0,0 +1,7 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LINK_TO_PRODUCT_PAGE = "https://www.saucedemo.com/"
+
+
